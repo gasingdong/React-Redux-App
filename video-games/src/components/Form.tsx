a5ea2e9aff0327e26e6e12830ea1e5c3ab80ff52
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-15 13:33:41 -0700

    sync form input to state

--- video-games/src/components/Form.tsx
+++ video-games/src/components/Form.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = (): React.ReactElement => {
+  const [name, setName] = useState('');
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setName(event.target.value);
+  };
+
   return (
     <div className="form">
       <form>
-        <input type="text" />
+        <input
+          type="text"
+          value={name}
+          id="name"
+          placeholder="Search by name"
+          onChange={changeHandler}
+        />
         <button type="submit">Search</button>
       </form>
     </div>
