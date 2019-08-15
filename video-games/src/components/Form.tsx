f55ef9deffd8836820673c7041fd5532088443a2
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-15 13:28:08 -0700

    add Form component

---
+++ video-games/src/components/Form.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = (): React.ReactElement => {
+  return (
+    <div className="form">
+      <form>
+        <input type="text" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
