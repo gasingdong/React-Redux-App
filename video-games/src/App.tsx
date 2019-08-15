0d122e9c8ebdd7b116765c019c569eb7b73b8fb7
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-15 13:30:13 -0700

    create GameList component

--- video-games/src/App.tsx
+++ video-games/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Form from './components/Form';
+import GameList from './components/GameList';
 
 const App: React.FC = (): React.ReactElement => {
   return (
     <div className="App">
       <Form />
+      <GameList />
     </div>
   );
 };
---
+++ video-games/src/components/GameList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GameList = (): React.ReactElement => {
+  return <div className="game-list" />;
+};
+
+export default GameList;
